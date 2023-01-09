7afca581718758ff1c2a49642aad2846af832fe1
oliver halvey <oliverhalvey310@yahoo.com>
2023-01-09 08:08:15 -0800

    Added Country Flag Images, plus Question and Answer structs

---
+++ Country Flag Game 2/Models/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  Country Flag Game 2
+//
+//  Created by Oliver Halvey on 1/9/23.
+//
+
+import Foundation
+struct Answer: Identifiable {
+    var id = UUID()
+    var text: String
+    var isCorrect: Bool
+}
+
+struct Question: Identifiable {
+    var id = UUID()
+    var correctAnswer: Answer
+    var incorrectAnswer: [Answer]
+}
