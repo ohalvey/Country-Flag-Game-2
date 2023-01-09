57619c4d995bb692a870a5a2cd6a263ec4918eb1
oliver halvey <oliverhalvey310@yahoo.com>
2023-01-09 07:51:15 -0800

    Initial Commit

---
+++ Country Flag Game 2/Country_Flag_Game_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Country_Flag_Game_2App.swift
+//  Country Flag Game 2
+//
+//  Created by Oliver Halvey on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Country_Flag_Game_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
