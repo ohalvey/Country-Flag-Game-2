045d9285473c0a26edccfb4ad7e7c8f443d65932
oliver halvey <oliverhalvey310@yahoo.com>
2023-01-10 07:59:33 -0800

    Added Custom Button, Progress Bar, and Answer Row

---
+++ Country Flag Game 2/Components/ProgressBar.swift
@@ -0,0 +1,31 @@
+//
+//  ProgressBar.swift
+//  Country Flag Game 2
+//
+//  Created by Oliver Halvey on 1/10/23.
+//
+
+import SwiftUI
+
+struct ProgressBar: View {
+    var progress: CGFloat
+    var body: some View {
+        ZStack(alignment: .leading) {
+                   Rectangle()
+                       .frame(maxWidth: 350, maxHeight: 4)
+                       .foregroundColor(.gray)
+                       .cornerRadius(10)
+                   Rectangle()
+                       .frame(width: progress, height: 4)
+                       .foregroundColor(.yellow)
+                       .cornerRadius(10)
+
+               }
+    }
+}
+
+struct ProgressBar_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressBar(progress: 50)
+    }
+}
