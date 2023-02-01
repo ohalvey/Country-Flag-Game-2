33bebc69c9143b630cf53f2152be0e5d1ee8f19d
oliver halvey <oliverhalvey310@yahoo.com>
2023-02-01 07:52:49 -0800

    Created Data and Quiz Manager

--- Country Flag Game 2/Models/Question.swift
+++ Country Flag Game 2/Models/Question.swift
@@ -15,5 +15,5 @@ struct Answer: Identifiable {
 struct Question: Identifiable {
     var id = UUID()
     var correctAnswer: Answer
-    var incorrectAnswer: [Answer]
+    var incorrectAnswers: [Answer]
 }
